ba8aa992aeb0ae0435cad90c41a8bebd0ee008cc
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-06-16 13:58:16 +0530

    commit

--- Q9_Jsontask.py
+++ Q9_Jsontask.py
@@ -5,3 +5,17 @@
 # • Read the file back
 # • Print only students with marks > 70
 # • Print total number of students
+import json
+student=[{"name":'Ravi','age':'20','city':'Bhopal','course':'DBMS','marks':50},
+        {"name":'Aman','age':'22','city':'Rewa','course':'ML','marks':60},
+        {"name":'Vikas','age':'23','city':'Dewas','course':'OS','marks':75},
+        {"name":'Raj','age':'24','city':'Pune','course':'CN','marks':80}]
+
+with open('Student.json','w') as f:
+    json.dump(student,f,indent=4)
+
+with open('Student.json','r') as f:
+    for i in f:
+        if(i['marks']>70):
+            print(i['name'])
+        
